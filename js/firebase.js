235991a1318294a411f6abb451b15ab1f7650880
TOMMASO DATI <tommaso.dati@ied.edu>
2019-03-18 10:16:08 +0100

    Aggiornamento

---
+++ js/firebase.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBZTuvwzRn6G2pZYZtTXp9MReceYlD-Dfo",
+  authDomain: "recepies-d822e.firebaseapp.com",
+  databaseURL: "https://recepies-d822e.firebaseio.com",
+  projectId: "recepies-d822e",
+  storageBucket: "recepies-d822e.appspot.com",
+  messagingSenderId: "478364802223"
+};
+firebase.initializeApp(config);
