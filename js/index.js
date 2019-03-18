a76ee756a279491fc41ac02c658a7eff56ea91cf
TOMMASO DATI <tommaso.dati@ied.edu>
2019-03-18 12:10:26 +0100

    Firebase

---
+++ js/index.js
@@ -0,0 +1,10 @@
+// JS for index
+
+var db = firebase.firestore();
+
+db.collection("recipies").get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+        //console.log(`${doc.id} => ${doc.data()}`);
+        console.log(doc);
+    });
+});
